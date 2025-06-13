19a238dcbf79dabf76e6d22f3cff78f9114c7e26
Mrunu2122 <mrunalsoshte212208@gmail.com>
2025-06-13 11:29:50 +0530

    Day 1

---
+++ case.py
@@ -0,0 +1,14 @@
+#Everything is Case sensitive
+course = 'Python for Beginners'
+#Upper case
+print(course.upper())
+#Original string
+print(course)
+#Lower case
+print(course.lower())
+#Finding index
+print(course.find('P'))
+#Replace
+print(course.replace('Beginners', 'Absolute beginners'))
+#Boolean
+print('Python' in course)
---
+++ operators.py
@@ -0,0 +1,20 @@
+print(10+2)#Plus
+print(10-2)#Minus
+print(10 / 3)#Division(Float value)
+print(10 // 3)#Division(int value)
+print(10 * 3)#Multiplication
+print(10 ** 3)#Power
+
+#Augmented assignment operator
+x = 10
+x = x + 3#(Normal addition)
+x += 3
+
+#precedance 
+x = 10 + 3 * 2 ** 2
+print(x)
+'''
+exponentiation(2 ** 3)
+multiplication or division
+addition or subtraction
+'''
